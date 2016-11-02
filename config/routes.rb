2d75671404f8c8838a27b0c28a05202c54b0aa5c
Son Tran <sontd.it@gmail.com>
2016-11-02 15:04:37 +0700

    Finish follow and unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users
+  resources :users, except: :delete
+  resources :relationships, only: [:create, :destroy]
   resources :categories, only: :index do
     resources :lessons, except: [:index, :new, :destroy]
   end
