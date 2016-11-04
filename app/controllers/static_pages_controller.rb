419671a1a3eff92e82c3292bc3bef813d12f5540
Son Tran <sontd.it@gmail.com>
2016-11-04 22:32:02 +0700

    Daily word and user bio

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,8 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @activities = Activity.user_activities(current_user).newest
         .paginate page: params[:page], per_page: Settings.activities_per_page
+      @word = Word.only_correct_meaning.random.take(Settings.daily_word).first
+      load_data current_user
     end
   end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,4 @@
+require "csv"
 
 class Word < ApplicationRecord
   belongs_to :category
